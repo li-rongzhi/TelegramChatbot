dc1e6a7cbd967cec79f9dad70d03fb3c6773ca5e
Rongzhi Li <e1088149@u.nus.edu>
2023-11-04 21:26:50 +0800

    Add get-news functionality

---
+++ features/news.py
@@ -0,0 +1,88 @@
+import os
+import requests
+from telegram import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import ContextTypes, ConversationHandler
+from utils.utils import NEWS, NEWS_CATEGORY, GET_NEWS, ENDPOINT
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+# Replace 'YOUR_API_KEY' with your actual News API key
+
+async def news(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Enters the news mode"""
+    user_id = update.message.from_user.id
+    db = context.user_data.get('db')
+    if db.check_user_state(user_id, NEWS):
+        await update.message.reply_text("You are already in the News mode")
+    else:
+        db.update_user_state(user_id, NEWS)
+        logger.info("User %s enters News mode", user_id)
+        await update.message.reply_text(
+            "You are now in the News mode. " + \
+            "Stay updated with the latest news. Use /get_news to fetch trending news online. \U0001F680"
+        )
+
+async def select_category(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Asks user to select a interested category"""
+    user_id = update.message.from_user.id
+    db = context.user_data.get('db')
+    if db.check_user_state(user_id, NEWS):
+        style_buttons = [[KeyboardButton(category)] for category in NEWS_CATEGORY]
+        style_markup = ReplyKeyboardMarkup(style_buttons, one_time_keyboard=True, resize_keyboard=True, input_field_placeholder="Please choose a style.")
+        await update.message.reply_text("Select a category:", reply_markup=style_markup)
+        return GET_NEWS
+    else:
+        await update.message.reply_text("Please start a Style Transfer mode using /style_transfer.")
+        return ConversationHandler.END
+
+async def get_news(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Gets popular news through NEWS API"""
+    context.user_data['category'] = update.message.text
+    logger.info("user chose: %s", update.message.text)
+    await update.message.reply_text('Great! Please wait a moment.', reply_markup=ReplyKeyboardRemove())
+    params = {
+        'apiKey': os.getenv("NEWS_API_KEY"),
+        'category': update.message.text,
+        'country': 'us'
+    }
+    try:
+        # Make the API request
+        response = requests.get(ENDPOINT, params=params)
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json()
+            # Check if there are articles in the response
+            if 'articles' in data:
+                articles = data['articles']
+                news_message = "<b>Here are the latest news articles:</b>\n\n"
+                # Display the top headlines
+                for article in articles[:3]:
+                    title = article.get('title', 'N/A')
+                    url = article.get('url')
+                    news_message += f"<a href='{url}'><b>{title}</b></a> \n"
+                    description = article.get('description')
+                    if description:
+                        news_message += f"<i>{description}</i>\n\n"
+                    else:
+                        news_message += "\n"
+                await update.message.reply_html(news_message)
+            else:
+                await update.message.reply_text("No articles found in the response.")
+        else:
+            await update.message.reply_text("No articles found in the response.")
+        return ConversationHandler.END
+    except Exception as e:
+        print("An error occurred:", e)
+        return ConversationHandler.END
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation in News.", user.first_name)
+    await update.message.reply_text(
+        "Bye!", reply_markup=ReplyKeyboardRemove()
+    )
+    return ConversationHandler.END
--- handler/handler.py
+++ handler/handler.py
@@ -3,21 +3,23 @@ from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandl
 import os
 from database.db import MySQLDatabase
 from features.llm import llm, get_output
+from features.news import get_news, select_category
 from features.task_managerment import add_task, delete_task, list_tasks, mark_task, task_management
 from features.timer import set_timer, unset
 from features.style_transfer.style_transfer import style_transfer, style, choose_style, upload_image, cancel
+import features.style_transfer.style_transfer as st
+import features.news as news
+from utils.utils import GENERAL, LLM, CHOOSE_STYLE, UPLOAD_IMAGE, STYLES, GET_NEWS, NEWS_CATEGORY
 
-from utils.utils import GENERAL, LLM, TASK_MANAGEMENT, TIMER, CHOOSE_STYLE, UPLOAD_IMAGE, STYLE_TRANSFER, STYLES
 
-
-db_password = os.environ.get('DB_PASSWORD')
-
-configs = {
-    'host':'localhost',
-    'user': 'root',
-    'password':db_password,
-    'database':'telebot'
-}
+# db_password = os.environ.get('DB_PASSWORD')
+# db_password = "Lrz133227!"
+# configs = {
+#     'host':'localhost',
+#     'user': 'root',
+#     'password':db_password,
+#     'database':'telebot'
+# }
 
 intro_message = """
 Welcome to Jarvis! Here are some of my functionalities:
@@ -28,12 +30,21 @@ Welcome to Jarvis! Here are some of my functionalities:
     - Chat freely with ChatGPT.
 3. <b>/style_transfer</b>
     - Transfer your image to predefined styles.
+4. <b>/news</b>
+    - Stay updated with the latest news.
 
 You can use these shortcuts to access each functionality.
 """
 
 # async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def start(update: Update, context: MySQLDatabase):
+    db_password = os.environ.get('DB_PASSWORD')
+    configs = {
+        'host':'localhost',
+        'user': 'root',
+        'password':db_password,
+        'database':'telebot'
+    }
     context.user_data['db'] = MySQLDatabase(**configs)
     user_id = update.message.from_user.id
     db = context.user_data.get('db')
@@ -86,15 +97,26 @@ def setup(application: Application):
 
     # style transfer functionality
     application.add_handler(CommandHandler("style_transfer", style_transfer))
-    conv_handler = ConversationHandler(
+    style_transfer_handler = ConversationHandler(
         entry_points=[CommandHandler('style', style)],
         states={
             CHOOSE_STYLE: [MessageHandler(filters.Text(STYLES), choose_style)],
             UPLOAD_IMAGE: [MessageHandler(filters.PHOTO, upload_image)],
         },
-        fallbacks=[CommandHandler("cancel", cancel)]
+        fallbacks=[CommandHandler("cancel", st.cancel)]
     )
-    application.add_handler(conv_handler)
+    application.add_handler(style_transfer_handler)
+
+    # news catcher functionality
+    application.add_handler(CommandHandler("news", news.news))
+    news_handler = ConversationHandler(
+        entry_points=[CommandHandler('get_news', select_category)],
+        states={
+            GET_NEWS: [MessageHandler(filters.Text(NEWS_CATEGORY), get_news)],
+        },
+        fallbacks=[CommandHandler("cancel", news.cancel)]
+    )
+    application.add_handler(news_handler)
 
     # llm functionality
     application.add_handler(CommandHandler("llm", llm))
